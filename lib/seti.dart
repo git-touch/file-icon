c9eab3ee12b57ea535badec60bbcff3456c0ebff
Rongjian Zhang <pd4d10@gmail.com>
2019-09-09 15:32:13 +0800

    feat: add widget to show icon by file name

--- lib/seti.dart
+++ lib/seti.dart
@@ -1,7 +1,29 @@
-library seti;
+import 'package:flutter/widgets.dart';
+import 'data.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class SetiIcon extends StatelessWidget {
+  final String fileName;
+  final bool light;
+  final double size;
+
+  SetiIcon(String fileName, {this.light = false, this.size})
+      : this.fileName = fileName.toLowerCase();
+
+  @override
+  Widget build(BuildContext context) {
+    final meta = light ? setiLightMeta : setiMeta;
+    String key;
+
+    if (meta['names'].containsKey(fileName)) {
+      key = meta['names'][fileName];
+    } else {
+      var ext = fileName.split('.').last;
+      if (meta['extensions'].containsKey(ext)) {
+        key = meta['extensions'][ext];
+      }
+    }
+
+    if (key == null) return null;
+    return iconDefinitions[key];
+  }
 }
