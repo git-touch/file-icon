e5c2395b0aa64826ff6740bed64e9da976d947f9
Rongjian Zhang <pd4d10@gmail.com>
2019-09-10 21:01:47 +0800

    feat(gallery): adjust name and size

--- seti-gallery/lib/main.dart
+++ seti-gallery/lib/main.dart
@@ -1,37 +1,72 @@
+import 'dart:html' as html;
 import 'package:flutter_web/material.dart';
+import 'generated/seti.dart';
 
 void main() => runApp(MyApp());
+
+const title = 'Seti icons for Flutter';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: title,
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  final String title;
+class _MyHomePageState extends State<MyHomePage> {
+  String _input = 'main.dart';
+  double _size = 100;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
+        actions: <Widget>[
+          InkWell(
+            child: SetiIcon('.html', size: 32),
+            onTap: () {
+              html.window.open('https://github.com/pd4d10/seti-flutter', '');
+            },
+          )
+        ],
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Hello, World!',
+            TextField(
+              onChanged: (v) {
+                _input = v;
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'File name',
+                labelText: 'Input file name here',
+              ),
+            ),
+            Slider(
+              value: _size,
+              onChanged: (v) {
+                setState(() {
+                  _size = v;
+                });
+              },
+              max: 300,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[SetiIcon(_input, size: _size)],
+              ),
             ),
           ],
         ),
