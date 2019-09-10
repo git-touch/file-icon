f37b6e0e2744d1df685fce210fda4948f69d9d43
Rongjian Zhang <pd4d10@gmail.com>
2019-09-10 20:13:45 +0800

    chore: stagehand init

---
+++ seti-gallery/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_web/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello, World!',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
