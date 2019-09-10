c338d8a66ba9ff6e9b947a5da2c8d38dfb5a188b
Rongjian Zhang <pd4d10@gmail.com>
2019-09-10 20:28:01 +0800

    build: generate lib for gallery

---
+++ tool/gulpfile.esm.js
@@ -0,0 +1,30 @@
+import gulp from "gulp";
+import through2 from "through2";
+
+function gallery() {
+  return gulp
+    .src("../seti/lib/**/*")
+    .pipe(
+      through2.obj((file, _, cb) => {
+        if (file.isBuffer()) {
+          file.contents = Buffer.from(
+            file.contents
+              .toString()
+              .replace("package:flutter", "package:flutter_web"),
+            "utf8"
+          );
+        }
+        cb(null, file);
+      })
+    )
+    .pipe(gulp.dest("../seti-gallery/lib/generated"));
+}
+
+export function watch() {
+  return gulp.watch("../seti/lib/**/*", { ignoreInitial: false }, gallery);
+}
+
+export default cb => {
+  gallery();
+  cb();
+};
